aacabb71cc6df83e6afb5a0ae9e19af258587529
solly29 <cksdud1311@naver.com>
2022-06-09 16:03:56 +0900

    2022-06-09 메인 todo card 리사이클러뷰 추가 아이템 구현

--- app/src/main/java/com/example/solly/todo/presentation/adapter/MainTodoItemAdapter.kt
+++ app/src/main/java/com/example/solly/todo/presentation/adapter/MainTodoItemAdapter.kt
@@ -8,29 +8,53 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.solly.todo.R
+import com.example.solly.todo.databinding.FragmentTodoAddItemBinding
 import com.example.solly.todo.databinding.FragmentTodoItemBinding
 
-class MainTodoItemAdapter: RecyclerView.Adapter<MainTodoItemAdapter.MainTodoItemViewHolder>() {
+class MainTodoItemAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val list = listOf("1번","2번","3번","4번")
 
+    private final val TODO_CARD_ITEM = 1
+    private final val TODO_CARD_ADD_ITEM = 2
+
     inner class MainTodoItemViewHolder(val item: FragmentTodoItemBinding): RecyclerView.ViewHolder(item.root) {
-        fun onBind(str: String) {
-            item.textViewTodoItem.text = str
+        fun onBind(data: String) {
+            item.textViewTodoItem.text = data
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainTodoItemViewHolder {
-        val item = DataBindingUtil.inflate<FragmentTodoItemBinding>(LayoutInflater.from(parent.context), R.layout.fragment_todo_item, parent, false)
-
-        return MainTodoItemViewHolder(item)
+    inner class MainTodoItemAddViewHolder(val item: FragmentTodoAddItemBinding): RecyclerView.ViewHolder(item.root) {
+        fun onBind(data: String) {
+        }
     }
 
-    override fun onBindViewHolder(holder: MainTodoItemViewHolder, position: Int) {
-        holder.onBind(list[position])
+    override fun getItemViewType(position: Int): Int {
+        return if(position == list.size) {
+            TODO_CARD_ADD_ITEM
+        } else{
+            TODO_CARD_ITEM
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if(viewType != TODO_CARD_ADD_ITEM) {
+            val item = DataBindingUtil.inflate<FragmentTodoItemBinding>(LayoutInflater.from(parent.context), R.layout.fragment_todo_item, parent, false)
+            MainTodoItemViewHolder(item)
+        } else {
+            val item = DataBindingUtil.inflate<FragmentTodoAddItemBinding>(LayoutInflater.from(parent.context), R.layout.fragment_todo_add_item, parent, false)
+            MainTodoItemAddViewHolder(item)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is MainTodoItemViewHolder)
+            holder.onBind(list[position])
+        else if(holder is MainTodoItemAddViewHolder)
+            holder.onBind("")
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return list.size + 1
     }
 }
